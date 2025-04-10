991e5e895a0022109aaf7261749734a7b74b9f41
Zaal Panthaki <zaalpanthaki@Zaals-MacBook-Air.local>
2025-04-10 09:58:45 -0400

    Add Next.js config to allow embedding in frames

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Allow embedding in frames
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://warpcast.com https://*.vercel.app https://*.farcaster.xyz *;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
